40ce1d0b9859ab8d3072870e3ad63e5ca9d2d6b0
Tiago Ferreira <tacf@users.noreply.github.com>
2026-03-29 12:33:06 +0100

    feat: pixelated look (#4)

--- src/world/mesher.h
+++ src/world/mesher.h
@@ -3,10 +3,13 @@
 
 #include <raylib.h>
 #include "world/chunk.h"
+#include <stdbool.h>
 
 struct World;
 
 void Mesher_RebuildChunk(struct World *world, Chunk *chunk, float ambient_multiplier);
+void Mesher_SetGeometryPixelSnapEnabled(bool enabled);
+void Mesher_SetGeometryPixelSnapResolution(float width, float height);
 void Mesher_Shutdown(void);
 
 #endif
